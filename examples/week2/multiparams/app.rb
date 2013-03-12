b20b81313270931bf34b2f98799670f76a8a1156
Zevensuy Rodriguez <zevenwolf@gmail.com>
2013-03-11 22:52:12 -0400

    loop

--- examples/week2/multiparams/app.rb
+++ examples/week2/multiparams/app.rb
@@ -7,6 +7,6 @@ end
 
 # Second route  - this is the form is posted to
 post '/multiparams' do
-  allnames = params[:names]
-  "Hello " + allnames['name'] + " " + allnames['name1'] + " " + allnames['name2'] + " " + allnames['name3']
+  @allnames = params[:names]
+  erb :multiparams
 end
